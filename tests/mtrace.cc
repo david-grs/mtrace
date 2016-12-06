ca24de35ef9f9d678b57daa994043e8e7647adc1
David G <david.a.grs@gmail.com>
2016-12-06 22:18:39 +0100

    adding unit tests

--- tests/mtrace.cc
+++ tests/mtrace.cc
@@ -1,22 +1,49 @@
 #include "mtrace.h"
 #include "malloc_chrono.h"
+#include "malloc_counter.h"
 
-#include <iostream>
+#include <gtest/gtest.h>
+
 #include <memory>
 
-int main()
+TEST(mtrace, counter)
 {
-    mtrace<malloc_chrono> mt;
+    mtrace<malloc_counter> mc;
 
     {
         auto uptr = std::make_unique<int>(5);
         (void)uptr;
 
-        std::cout << "malloc=" << std::get<0>(mt).elapsed_time_malloc().count() << std::endl;
-        std::cout << "free=" << std::get<0>(mt).elapsed_time_free().count() << std::endl;
+        EXPECT_EQ(1, std::get<0>(mc).malloc_calls());
+        EXPECT_EQ(0, std::get<0>(mc).free_calls());
+        EXPECT_EQ(0, std::get<0>(mc).realloc_calls());
     }
 
-    std::cout << "malloc=" << std::get<0>(mt).elapsed_time_malloc().count() << std::endl;
-    std::cout << "free=" << std::get<0>(mt).elapsed_time_free().count() << std::endl;
+    EXPECT_EQ(1, std::get<0>(mc).malloc_calls());
+    EXPECT_EQ(1, std::get<0>(mc).free_calls());
+    EXPECT_EQ(0, std::get<0>(mc).realloc_calls());
+}
+
+TEST(mtrace, chrono)
+{
+    mtrace<malloc_chrono> mc;
+    int64_t malloc_time;
+
+    {
+        auto uptr = std::make_unique<int>(5);
+        (void)uptr;
+
+        malloc_time = std::get<0>(mc).elapsed_time_malloc().count();
+        EXPECT_GE(malloc_time, 1);
+        EXPECT_EQ(0, std::get<0>(mc).elapsed_time_free().count());
+        EXPECT_EQ(0, std::get<0>(mc).elapsed_time_realloc().count());
+    }
+
+
+    EXPECT_EQ(malloc_time, std::get<0>(mc).elapsed_time_malloc().count());
+
+    int64_t free_time = std::get<0>(mc).elapsed_time_free().count();
+    EXPECT_GE(free_time, 1);
+    EXPECT_EQ(0, std::get<0>(mc).elapsed_time_realloc().count());
 }
 
