ded7f987026aa5bbb8516f1f6b15e76e92eb9bd2
David G <david.a.grs@gmail.com>
2017-01-01 18:49:40 +0100

    tsc_chrono: adding chip,core params to rdtscp(), throw if executable not set to specific core, fix clock

--- tsc_chrono.h
+++ tsc_chrono.h
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <cmath>
+#include <type_traits>
 
 namespace detail
 {
@@ -21,6 +22,15 @@ static inline uint64_t rdtscp()
     return ((uint64_t)rdx << 32) + (uint64_t)rax;
 }
 
+static inline uint64_t rdtscp(int& chip, int& core)
+{
+    uint32_t rax, rdx, rcx;
+    __asm__ __volatile__("rdtscp" : "=a"(rax), "=d"(rdx), "=c"(rcx));
+    chip = (rcx & 0xFFF000) >> 12;
+    core = rcx & 0xFFF;
+    return ((uint64_t)rdx << 32) + (uint64_t)rax;
+}
+
 struct tsc
 {
     static double& get_freq_ghz()
@@ -36,15 +46,22 @@ inline void init()
     if (tsc_freq_ghz)
         return;
 
-    using clock = std::chrono::high_resolution_clock;
+    using Clock = std::conditional_t<std::chrono::high_resolution_clock::is_steady,
+                                     std::chrono::high_resolution_clock,
+                                     std::chrono::steady_clock>;
 
-    auto start = clock::now();
+    int chip, core, chip2, core2;
 
-    uint64_t rdtsc_start = detail::rdtsc();
+    auto start = Clock::now();
+    uint64_t rdtsc_start = detail::rdtscp(chip, core);
+
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    uint64_t rdtsc_end = detail::rdtsc();
 
-    auto end = clock::now();
+    uint64_t rdtsc_end = detail::rdtscp(chip2, core2);
+    auto end = Clock::now();
+
+    if (core != core2 || chip != chip2)
+        throw std::runtime_error("please set this executable to a specific core");
 
     auto duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     uint64_t cycles = rdtsc_end - rdtsc_start;
@@ -76,6 +93,14 @@ struct tsc_chrono
     int64_t elapsed() const
     {
         return detail::rdtsc() - m_start;
+    }
+
+    int64_t elapsed_and_restart()
+    {
+        uint64_t now = detail::rdtsc();
+        int64_t ts = now - m_start;
+        m_start = now;
+        return ts;
     }
 
     std::chrono::nanoseconds elapsed_time() const
